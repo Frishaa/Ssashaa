6c55cef4dd1aa9649761dc3af0ce718b1513bdf5
Frishaa <sasharamadhani34@gmail.com>
2025-05-26 22:43:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("😶‍🌫️Selamat datang di web Ssashaa")
+st.title("😶‍🌫️Selamat datang di web Ssashaa🤐😶‍🌫️")
 st.write(
     "Thank you for click this web to see my new web"
 )
